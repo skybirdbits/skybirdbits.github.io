2b5a3753a9b3856b0a69909b5d9480324d103fe6
skybirdbits <skybird.bits@gmail.com>
2024-03-18 18:40:22 +0000

    Updated

--- resources/js/article.js
+++ resources/js/article.js
@@ -153,7 +153,7 @@ function showPageLastModified(){
   var ltu = document.querySelector(".page-last-update");
 
   if(ltu != null)
-    ltu.innerHTML = ltu.innerHTML + " " + date;
+    ltu.innerHTML = "تاریخ بروزرسانی: " + date;
 
 }
 
