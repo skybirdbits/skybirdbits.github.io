77cea98e0eb96c13b081a5b89ee16a18ca399f6f
Younes <skybirdbits@gmail.com>
2025-09-02 02:41:02 +0330

    dialog added to the files

--- resources/js/base.js
+++ resources/js/base.js
@@ -11,6 +11,7 @@ const courseDataRepo = [
 
 const header = document.querySelector('header');
 const footer = document.querySelector("footer");
+const dialog = document.querySelector('.dialog');
 
 class CourseData {
   isExpanded = false;
@@ -28,7 +29,6 @@ class CourseData {
 
 //loads Ui Components into documents
 function loadAllUiComponents() {
-
 
   loadComponent("/ui-components/header-contents.html", header, () => {
 
@@ -51,6 +51,10 @@ function loadAllUiComponents() {
   });
 
   loadComponent("/ui-components/footer-contents.html", footer);
+
+  loadComponent("/ui-components/dialog-contents.html", dialog, () => {
+        initDialog();
+  });
 
 }
 
@@ -119,6 +123,48 @@ var scrollTopVisibility = function () {
   }
 
   currentScrollY = window.scrollY;
+}
+
+const KEY_VISIBILITY_STATUS = 'dialog-visibility-status-key';
+
+function hideDialog(){
+    dialog.classList.remove('show');
+    dialog.classList.add('hide');
+}
+
+function showDialog(){
+    dialog.classList.remove('hide');
+    dialog.classList.add('show');
+}
+
+function initDialog(){
+    //localStorage.removeItem(KEY_VISIBILITY_STATUS);
+    var keepHidden = JSON.parse(localStorage.getItem(KEY_VISIBILITY_STATUS));
+
+    if(keepHidden != null){
+        if(keepHidden) {
+            return;
+        }
+    }
+
+    showDialog();
+
+    var btClose = document.getElementById('bt-close-dialog');
+
+    btClose.onclick = function(){ hideDialog(); }
+
+    var checkboxStatus = document.getElementById('status');
+
+    var btConfirm = document.getElementById('bt-confirm');
+    btConfirm.onclick = function(){
+
+        keepHidden = checkboxStatus.checked;
+
+        localStorage.setItem(KEY_VISIBILITY_STATUS, JSON.stringify(keepHidden));
+        hideDialog();
+
+    }
+
 }
 
 export {loadAllUiComponents, scrollTopVisibility};
